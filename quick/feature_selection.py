e4d58a9ebb1dbc7decd40656739e97ac3f2d7360
Nicolas Wagner <nicolas.wagner@epfl.ch>
2024-04-28 17:39:54 +0200

    correct mistake

--- quick/feature_selection.py
+++ quick/feature_selection.py
@@ -3,19 +3,19 @@ from sklearn.datasets import fetch_openml
 from sklearn.feature_selection import RFECV
 from sklearn.linear_model import Ridge
 from sklearn.metrics import mean_squared_error, make_scorer
-from sklearn.model_selection import train_test_split
 
 from quick.utils import get_mse
 
-# Load data from https://www.openml.org/d/554
-
 print(f'MSE before: {get_mse()}')
+
+X, y = fetch_openml("mnist_784", version=1, return_X_y=True, as_frame=False)
+y = y.astype(np.uint8)
+X = X / 255.
 
 rr = Ridge(solver='svd')
 sfs = RFECV(rr, scoring=make_scorer(mean_squared_error), n_jobs=-1)
+sfs.fit(X, y)
 mask = sfs.get_support()
 transform_X = lambda X: X[:, mask]
 
 print(f'MSE after: {get_mse(transform_X=transform_X)}')
-
-
